03d03fbdf7d79f0193355b7d05394e6c2c105473
Pedro Henrique Eifler <62551886+pedroEifler@users.noreply.github.com>
2021-10-05 20:16:47 -0300

    Calculando binario e hexadecimal

--- src/front/javascript/script.js
+++ src/front/javascript/script.js
@@ -1,26 +1,41 @@
 function calcular() {
-    var valorEsquerdo = parseInt(document.getElementById("inputValorEsquerdo").value, 2);
-    var valorDireito = parseInt(document.getElementById("inputValorDireito").value, 2);
-    var operador = document.getElementById("operador").value;
-    var resultadoDecimal = document.getElementById("resultadoDecimal");
-    var resultadoBinario = document.getElementById("resultadoBinario");
-    var result = 0;
+    /* Tipos (Bin|Hex) == (2|16) */
+    const tipoEsquerdo = document.getElementById("tipoEsquerdo").value;
+    const tipoDireito = document.getElementById("tipoDireito").value;
+
+    /* Valores digitados pelo usuário */
+    const valorEsquerdo = document.getElementById("inputValorEsquerdo").value;
+    const valorDireito = document.getElementById("inputValorDireito").value;
+
+    /* Sinal escolhido pelo usuário (+|-|x|/) == (1|2|3|4) */
+    const operador = document.getElementById("operador").value;
+
+    /* Valores convertidos para decimal pra conseguir calcular */
+    const valorEsquerdoDecimal = converterParaDecimal(valorEsquerdo, tipoEsquerdo);
+    const valorDireitoDecimal = converterParaDecimal(valorDireito, tipoDireito)
+
+    let resultado = 0;
 
     switch (operador) {
         case '1':
-            result = valorEsquerdo + valorDireito;
+            resultado = valorEsquerdoDecimal + valorDireitoDecimal;
             break;
         case '2':
-            result = valorEsquerdo - valorDireito;
+            resultado = valorEsquerdoDecimal - valorDireitoDecimal;
             break;
         case '3':
-            result = valorEsquerdo * valorDireito;
+            resultado = valorEsquerdoDecimal * valorDireitoDecimal;
             break;
         case '4':
-            result = valorEsquerdo / valorDireito;
+            resultado = valorEsquerdoDecimal / valorDireitoDecimal;
             break;
     }
 
-    resultadoDecimal.textContent = result
-    resultadoBinario.textContent = result.toString(2)
+    document.getElementById("resultadoDecimal").textContent = resultado
+    document.getElementById("resultadoBinario").textContent = resultado.toString(2)
+    document.getElementById("resultadoHexadecimal").textContent = resultado.toString(16)
+}
+
+function converterParaDecimal(valor, tipo) {
+    return parseInt(valor, tipo)
 }
